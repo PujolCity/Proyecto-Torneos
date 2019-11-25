78f6405c21c7b233bfa35a4d465072fa5b274b2d
alex <alex6tc90@gmail.com>
2019-11-25 17:07:58 -0300

    Finalizada tabla de posiciones en pantalla Posiciones de detalle de la competencia.

--- app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/PosicionesFragment.java
+++ app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/PosicionesFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.Gravity;
@@ -15,8 +16,24 @@ import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.VeizagaTorrico.proyectotorneos.R;
+import com.VeizagaTorrico.proyectotorneos.RetrofitAdapter;
+import com.VeizagaTorrico.proyectotorneos.models.CompetitionMin;
+import com.VeizagaTorrico.proyectotorneos.models.Confrontation;
+import com.VeizagaTorrico.proyectotorneos.models.PositionCompetitor;
+import com.VeizagaTorrico.proyectotorneos.services.CompetitionSrv;
+import com.VeizagaTorrico.proyectotorneos.services.ConfrontationSrv;
+import com.VeizagaTorrico.proyectotorneos.services.PositionCompetitorSrv;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PosicionesFragment extends Fragment {
 
@@ -24,12 +41,17 @@ public class PosicionesFragment extends Fragment {
 
     private View vista;
 
+    private PositionCompetitorSrv positionSrv;
+
     private TableLayout tablaPosiciones;
+    private CompetitionMin competition;
+    private List<PositionCompetitor> posiciones;
 
     public PosicionesFragment() {
         // Required empty public constructor
     }
-public static PosicionesFragment newInstance() {
+
+    public static PosicionesFragment newInstance() {
         PosicionesFragment fragment = new PosicionesFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -47,48 +69,96 @@ public static PosicionesFragment newInstance() {
         // Inflate the layout for this fragment
         vista = inflater.inflate(R.layout.fragment_posiciones, container, false);
         tablaPosiciones = vista.findViewById(R.id.TablaPosiciones);
-        showTablePositions();
+        initElements();
+        callTablePosition(competition.getId());
         return vista;
     }
 
-    private void showTablePositions() {
+    private void initElements() {
+        positionSrv = new RetrofitAdapter().connectionEnable().create(PositionCompetitorSrv.class);
+    }
 
-        Log.d("LOG - ADD", "Entramos a la funcion para agregar la fila");
+    private void callTablePosition(int idCompetencia){
+        Call<List<PositionCompetitor>> call = positionSrv.getTablePositions(idCompetencia);
+        try{
+            call.enqueue(new Callback<List<PositionCompetitor>>() {
+                @Override
+                public void onResponse(Call<List<PositionCompetitor>> call, Response<List<PositionCompetitor>> response) {
+                    if(response.code() == 200){
+                        posiciones = response.body();
+                    }
+                    // si recibimos los resultados de las posiciones de los competidores
+                    if(posiciones != null){
+                        Collections.sort(posiciones, new Comparator<PositionCompetitor>() {
+                            @Override
+                            public int compare(PositionCompetitor pos1, PositionCompetitor pos2) {
+                                return pos1.getPuntos().compareTo(pos2.getPuntos());
+                            }
+                        });
+                        Collections.reverse(posiciones);
+                        showTablePositions(posiciones);
+                    }
+                }
 
+                @Override
+                public void onFailure(Call<List<PositionCompetitor>> call, Throwable t) {
+                    Log.d("On failure", t.getMessage());
+                    Toast toast = Toast.makeText(vista.getContext(), "No se pudieron recuperar las posiciones de la competencia", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showTablePositions(List<PositionCompetitor> posiciones) {
+        // agregamos los resultados a la tabla
+        for (int i = 0; i < posiciones.size(); i++) {
+            TableRow resultadoCompetidor = getRowTable(posiciones.get(i));
+            tablaPosiciones.addView(resultadoCompetidor);
+        }
+
+    }
+
+    // crea una nueva fila para la tabla de resultados desde la info recuperada del servidor
+    private TableRow getRowTable(PositionCompetitor posicionCompetidor){
+        // creamos una fila vacia
         TableRow tbrow = new TableRow(getContext());
+
         // agregamos los valores de las columnas
         TextView tvCompetidor = new TextView(getContext());
-        tvCompetidor.setText("CompX");
+        tvCompetidor.setText(posicionCompetidor.getCompetidor());
         tvCompetidor.setTextColor(Color.BLACK);
         tvCompetidor.setGravity(Gravity.CENTER);
         tbrow.addView(tvCompetidor);
         TextView tvPj = new TextView(getContext());
-        tvPj.setText("X");
+        tvPj.setText(posicionCompetidor.getJugados());
         tvPj.setTextColor(Color.BLACK);
         tvPj.setGravity(Gravity.CENTER);
         tbrow.addView(tvPj);
         TextView tvPg = new TextView(getContext());
-        tvPg.setText("X");
+        tvPg.setText(posicionCompetidor.getGanados());
         tvPg.setTextColor(Color.BLACK);
         tvPg.setGravity(Gravity.CENTER);
         tbrow.addView(tvPg);
         TextView tvPe = new TextView(getContext());
-        tvPe.setText("X");
+        tvPe.setText(posicionCompetidor.getEmpatados());
         tvPe.setTextColor(Color.BLACK);
         tvPe.setGravity(Gravity.CENTER);
         tbrow.addView(tvPe);
         TextView tvPp = new TextView(getContext());
-        tvPp.setText("X");
+        tvPp.setText(posicionCompetidor.getPerdidos());
         tvPp.setTextColor(Color.BLACK);
         tvPp.setGravity(Gravity.CENTER);
         tbrow.addView(tvPp);
         TextView tvPts = new TextView(getContext());
-        tvPts.setText("X");
+        tvPts.setText(posicionCompetidor.getPuntos());
         tvPts.setTextColor(Color.BLACK);
         tvPts.setGravity(Gravity.CENTER);
         tbrow.addView(tvPts);
 
-        tablaPosiciones.addView(tbrow);
+        return tbrow;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -106,6 +176,10 @@ public static PosicionesFragment newInstance() {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+    }
+
+    public void setCompetencia(CompetitionMin competition) {
+        this.competition = competition;
     }
 
     @Override
