725dc990d3afc71bb23ee8df6fcaa0fc6d536b23
alexn6 <alex6tc90@gmail.com>
2019-11-25 11:15:24 -0300

    Incorporada tabla de posiciones de prueba.

--- app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/PosicionesFragment.java
+++ app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/PosicionesFragment.java
@@ -1,14 +1,20 @@
 package com.VeizagaTorrico.proyectotorneos.fragments.detalle_competencias;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.VeizagaTorrico.proyectotorneos.R;
 
@@ -17,6 +23,8 @@ public class PosicionesFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     private View vista;
+
+    private TableLayout tablaPosiciones;
 
     public PosicionesFragment() {
         // Required empty public constructor
@@ -38,7 +46,49 @@ public static PosicionesFragment newInstance() {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         vista = inflater.inflate(R.layout.fragment_posiciones, container, false);
+        tablaPosiciones = vista.findViewById(R.id.TablaPosiciones);
+        showTablePositions();
         return vista;
+    }
+
+    private void showTablePositions() {
+
+        Log.d("LOG - ADD", "Entramos a la funcion para agregar la fila");
+
+        TableRow tbrow = new TableRow(getContext());
+        // agregamos los valores de las columnas
+        TextView tvCompetidor = new TextView(getContext());
+        tvCompetidor.setText("CompX");
+        tvCompetidor.setTextColor(Color.BLACK);
+        tvCompetidor.setGravity(Gravity.CENTER);
+        tbrow.addView(tvCompetidor);
+        TextView tvPj = new TextView(getContext());
+        tvPj.setText("X");
+        tvPj.setTextColor(Color.BLACK);
+        tvPj.setGravity(Gravity.CENTER);
+        tbrow.addView(tvPj);
+        TextView tvPg = new TextView(getContext());
+        tvPg.setText("X");
+        tvPg.setTextColor(Color.BLACK);
+        tvPg.setGravity(Gravity.CENTER);
+        tbrow.addView(tvPg);
+        TextView tvPe = new TextView(getContext());
+        tvPe.setText("X");
+        tvPe.setTextColor(Color.BLACK);
+        tvPe.setGravity(Gravity.CENTER);
+        tbrow.addView(tvPe);
+        TextView tvPp = new TextView(getContext());
+        tvPp.setText("X");
+        tvPp.setTextColor(Color.BLACK);
+        tvPp.setGravity(Gravity.CENTER);
+        tbrow.addView(tvPp);
+        TextView tvPts = new TextView(getContext());
+        tvPts.setText("X");
+        tvPts.setTextColor(Color.BLACK);
+        tvPts.setGravity(Gravity.CENTER);
+        tbrow.addView(tvPts);
+
+        tablaPosiciones.addView(tbrow);
     }
 
     public void onButtonPressed(Uri uri) {
