58b2eb54353b7d3353809561d6845b3498d0f3a8
alex <alex6tc90@gmail.com>
2020-02-26 17:43:38 -0300

    Incorporado ocultar inscripcion si no esta creada. Agregado texto de misSolicitudes.

--- app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/InfoGeneralCompetenciaFragment.java
+++ app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/detalle_competencias/InfoGeneralCompetenciaFragment.java
@@ -229,6 +229,15 @@ public class InfoGeneralCompetenciaFragment extends Fragment {
     private void ocultarBotones(){
         List<String> roles = this.competition.getRol();
 
+        // ocultamos el boton de inscripcion si la inscripcion no esta abierta
+        if(this.competition.getEstado().contains("COMPETENCIA_INSCRIPCION_ABIERTA")){
+            inscribirse.setVisibility(View.VISIBLE);
+        }
+        else{
+            inscribirse.setVisibility(View.INVISIBLE);
+        }
+
+
         for (int i = 0 ; i < roles.size(); i++) {
             if (roles.get(i).contains("SOLICITANTE")) {
                 follow.setVisibility(View.INVISIBLE);
