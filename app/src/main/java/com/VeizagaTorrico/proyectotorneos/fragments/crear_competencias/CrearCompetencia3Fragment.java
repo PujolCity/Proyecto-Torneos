fabae7d32541572a7da2192dbd01950c0e3280a8
alex <alex6tc90@gmail.com>
2020-04-09 16:45:50 -0300

    Corregido control de cant maxima de competidores a la hora de crear competencias.

--- app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/crear_competencias/CrearCompetencia3Fragment.java
+++ app/src/main/java/com/VeizagaTorrico/proyectotorneos/fragments/crear_competencias/CrearCompetencia3Fragment.java
@@ -53,17 +53,16 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
 
     private LinearLayout linl_fase, linl_grupo, linl_max_comp;
     private Button btnCrear;
-    private Spinner spinner, spinnerFase;
+    private Spinner spinner, spinnerFase, spinnerGrupo;
     private TextView txtView, tvGrupo, tvMaxComp;
-    private EditText etGrupo;
     private EditText etMaxCompetidores;
     private Competition competition;
     private TypesOrganizationSrv orgSrv;
     private CompetitionSrv competitionSrv;
     private View vista;
     private Map<String,String> competencia;
-    private String cantGrupos;
-    private int fase;
+    private String maxCompetidores;
+    private int fase, cantGrupo;
     private boolean hayGrupo, hayFase, hayMaxComp;
     private int usuario;
 
@@ -102,85 +101,101 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
         btnCrear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    competencia.put("nombre",competition.getName());
-                    competencia.put("fecha_ini",competition.getFechaInicio());
-                    competencia.put("ciudad", Integer.toString(competition.getCiudad().getId()));
-                    competencia.put("genero",competition.getGenero());
-                    competencia.put("categoria_id", Integer.toString(competition.getCategory().getId()));
-                    competencia.put("tipoorg_id",Integer.toString(competition.getTypesOrganization().getId()));
-                    competencia.put("user_id",Integer.toString(usuario));
-                    competencia.put("frecuencia",Integer.toString(competition.getFrecuencia()));
-                    Log.d("BODY" , competencia.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if(hayGrupo) {
-                    cantGrupos = etGrupo.getText().toString();
-                    if(validarGrupo()){
-                        competencia.put("cant_grupos", cantGrupos);
-                    }else{
-                        Toast.makeText(getContext(), "Por favor complete los campos vacios", Toast.LENGTH_SHORT);
-                    }
-                }
-                else {
-                    competencia.put("cant_grupos", null);
-                }
-                if(hayFase){
-                    competencia.put("fase", Integer.toString(fase));
-                }else {
-                    competencia.put("fase", null);
-                }
-                if(hayMaxComp){
-                    if(!Validations.isEmpty(etMaxCompetidores)){
-                        competencia.put("max_comp",etMaxCompetidores.getText().toString());
-                    }
-                    else{
-                        competencia.put("max_comp", null);
-                    }
-                }
+                if(recuperarValoresIngresados()){
+                    Call<Competition> call = competitionSrv.createCompetition(competencia);
+                    Log.d("Url",call.request().url().toString());
 
-                Call<Competition> call = competitionSrv.createCompetition(competencia);
-                Log.d("Url",call.request().url().toString());
+                    call.enqueue(new Callback<Competition>() {
+                        @Override
+                        public void onResponse(Call<Competition> call, Response<Competition> response) {
+                            Log.d("Response code",Integer.toString(response.code()));
 
-                call.enqueue(new Callback<Competition>() {
-                    @Override
-                    public void onResponse(Call<Competition> call, Response<Competition> response) {
-                        Log.d("Response code",Integer.toString(response.code()));
-
-                        if(response.code() == 201){
-                            Log.d("COMPETENCIA CREADA", competencia.toString());
-                            // ACA ES DONDE PUEDO PASAR A OTRO FRAGMENT Y DE PASO MANDAR UN OBJETO QUE CREE CON EL BUNDLE
-                            Toast toast = Toast.makeText(vista.getContext(), "Competencia Creada!", Toast.LENGTH_SHORT);
-                            toast.show();
-                            Navigation.findNavController(vista).navigate(R.id.misCompetencias);
-                        }
-                        if (response.code() == 400) {
-                            JSONObject jsonObject = null;
-                            try {
-                                jsonObject = new JSONObject(response.errorBody().string());
-                                String userMessage = jsonObject.getString("messaging");
-                                Log.d("CREATE_COMP", "Msg de la repuesta: "+userMessage);
-                                Toast.makeText(vista.getContext(), "Error en la peticion: "+userMessage+" >>", Toast.LENGTH_LONG).show();
-                            } catch (Exception e) {
-                                e.printStackTrace();
+                            if(response.code() == 201){
+                                Log.d("COMPETENCIA CREADA", competencia.toString());
+                                // ACA ES DONDE PUEDO PASAR A OTRO FRAGMENT Y DE PASO MANDAR UN OBJETO QUE CREE CON EL BUNDLE
+                                Toast toast = Toast.makeText(vista.getContext(), "Competencia Creada!", Toast.LENGTH_SHORT);
+                                toast.show();
+                                Navigation.findNavController(vista).navigate(R.id.misCompetencias);
                             }
-                            return;
+                            if (response.code() == 400) {
+                                JSONObject jsonObject = null;
+                                try {
+                                    jsonObject = new JSONObject(response.errorBody().string());
+                                    String userMessage = jsonObject.getString("messaging");
+                                    Log.d("CREATE_COMP", "Msg de la repuesta: "+userMessage);
+                                    Toast.makeText(vista.getContext(), "Error en la peticion: "+userMessage+" >>", Toast.LENGTH_LONG).show();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                return;
+                            }
+                            if (response.code() == 500) {
+                                Log.d("CREATE_COMP_ERROR", "Problemas en el servidor.");
+                                Toast.makeText(vista.getContext(), "Problemas en el servidor ", Toast.LENGTH_LONG).show();
+                            }
                         }
-                        if (response.code() == 500) {
-                            Log.d("CREATE_COMP_ERROR", "Problemas en el servidor.");
-                            Toast.makeText(vista.getContext(), "Problemas en el servidor ", Toast.LENGTH_LONG).show();
+                        @Override
+                        public void onFailure(Call<Competition> call, Throwable t) {
+                            Toast toast = Toast.makeText(vista.getContext(), "Problemas con el servidor", Toast.LENGTH_SHORT);
+                            toast.show();
                         }
-                    }
-                    @Override
-                    public void onFailure(Call<Competition> call, Throwable t) {
-                        Toast toast = Toast.makeText(vista.getContext(), "Problemas con el servidor", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
+                    });
+                }
             }
         });
 
+    }
+
+    // recuperamos los valores ingresados por el usuario
+    private boolean recuperarValoresIngresados() {
+        if(hayMaxComp){
+            maxCompetidores = etMaxCompetidores.getText().toString();
+            if(correctoMaxCompetidores(competition.getTypesOrganization().getName())){
+                if(competition.getTypesOrganization().getName().contains("grupo")){
+                    competencia.put("max_comp",String.valueOf(Integer.valueOf(maxCompetidores)*cantGrupo));
+                }
+                if(competition.getTypesOrganization().getName().contains("Liga")){
+                    competencia.put("max_comp",maxCompetidores);
+                }
+            }
+            else{
+                if(competition.getTypesOrganization().getName().contains("grupo")){
+                    Toast.makeText(vista.getContext(), "La cantidad de competidores debe estar entre 3 y 20 ", Toast.LENGTH_LONG).show();
+                }
+                if(competition.getTypesOrganization().getName().contains("Liga")){
+                    Toast.makeText(vista.getContext(), "La cantidad de competidores debe estar entre 3 y 80 ", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        }
+        else{
+            competencia.put("max_comp", null);
+        }
+
+        competencia.put("nombre",competition.getName());
+        competencia.put("fecha_ini",competition.getFechaInicio());
+        competencia.put("ciudad", Integer.toString(competition.getCiudad().getId()));
+        competencia.put("genero",competition.getGenero());
+        competencia.put("categoria_id", Integer.toString(competition.getCategory().getId()));
+        competencia.put("tipoorg_id",Integer.toString(competition.getTypesOrganization().getId()));
+        competencia.put("user_id",Integer.toString(usuario));
+        competencia.put("frecuencia",Integer.toString(competition.getFrecuencia()));
+
+        if(hayGrupo) {
+            competencia.put("cant_grupos", String.valueOf(cantGrupo));
+        }
+        else {
+            competencia.put("cant_grupos", null);
+        }
+
+        if(hayFase){
+            competencia.put("fase", Integer.toString(fase));
+        }else {
+            competencia.put("fase", null);
+        }
+
+        Log.d("BODY" , competencia.toString());
+        return true;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -208,8 +223,7 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
 
     @Override
     public void sinInternet() {
-        Toast toast = Toast.makeText(vista.getContext(), "Sin Conexion a Internet, por el momento no se podran crear competencias.", Toast.LENGTH_LONG);
-        toast.show();
+        Toast.makeText(vista.getContext(), "Sin Conexion a Internet, por el momento no se podran crear competencias.", Toast.LENGTH_LONG).show();
         btnCrear.setVisibility(View.INVISIBLE);
     }
 
@@ -225,18 +239,16 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
 
         txtView = vista.findViewById(R.id.descripcionTipoOrg);
         tvMaxComp = vista.findViewById(R.id.tv_mx_comp);
-        etGrupo = vista.findViewById(R.id.etCantGrupo);
-        etGrupo.setFilters(new InputFilter[]{ new InputFilterMinMax("2", "20")});
         etMaxCompetidores = vista.findViewById(R.id.etMaxComp);
         btnCrear = vista.findViewById(R.id.btnCCSig_3);
         spinner = vista.findViewById(R.id.spinnerTipoOrg);
         spinnerFase = vista.findViewById(R.id.spinnerFase);
+        spinnerGrupo = vista.findViewById(R.id.spinnerCantGrupos);
         tvGrupo = vista.findViewById(R.id.tv_grupo_crearComp3);
 
         linl_fase = vista.findViewById(R.id.linl_fase);
         linl_grupo = vista.findViewById(R.id.linl_grupo);
         linl_max_comp = vista.findViewById(R.id.linl_max_comp);
-
         hayMaxComp = false;
     }
 
@@ -277,8 +289,7 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
 
              @Override
              public void onFailure(Call<List<TypesOrganization>> call, Throwable t) {
-                 Toast toast = Toast.makeText(getContext(), "Por favor recargar pestaña", Toast.LENGTH_SHORT);
-                 toast.show();
+                 Toast.makeText(getContext(), "Por favor recargar pestaña", Toast.LENGTH_SHORT).show();
                  Log.d("onFailure", t.getMessage());
              }
          });
@@ -310,15 +321,37 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
         });
     }
 
+    private void llenarSpinnerGrupo() {
+        final List<Integer> numeros = new ArrayList<>();
+        for (int i=2; i<= 20; i++){
+            numeros.add(i);
+        }
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(vista.getContext(),android.R.layout.simple_spinner_item, numeros);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGrupo.setAdapter(adapter);
+        spinnerGrupo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                cantGrupo = i+2;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
     private void updateView(String typeOrg){
         if(typeOrg.contains("grupo")){
              //Log.d("prueba" , "entra?" );
              linl_max_comp.setVisibility(View.VISIBLE);
              linl_grupo.setVisibility(View.VISIBLE);
              hayGrupo = true;
+             llenarSpinnerGrupo();
              hayMaxComp = true;
-             tvMaxComp.setText("Cantidad por grupo(entre 3 y 20): ");
-             etMaxCompetidores.setFilters(new InputFilter[]{ new InputFilterMinMax("3", "20")});
+             tvMaxComp.setText("Competidores por grupo (entre 3 y 20): ");
          } else{
             linl_grupo.setVisibility(View.GONE);
              hayGrupo = false;
@@ -327,8 +360,8 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
          if(typeOrg.contains("Eliminatorias")){
              linl_max_comp.setVisibility(View.GONE);
              linl_fase.setVisibility(View.VISIBLE);
-             hayMaxComp = false;
              hayFase = true;
+             hayMaxComp = false;
              llenarSpinnerFase();
          } else{
              linl_fase.setVisibility(View.GONE);
@@ -336,16 +369,31 @@ public class CrearCompetencia3Fragment extends Fragment implements MensajeSinInt
          }
         if(typeOrg.contains("Liga")) {
             //Log.d("prueba" , "entra?" );
-            linl_max_comp.setVisibility(View.VISIBLE);
             hayMaxComp = true;
-            tvMaxComp.setText("Cantidad total(entre 3 y 80): ");
-            etMaxCompetidores.setFilters(new InputFilter[]{ new InputFilterMinMax("3", "80")});
+            linl_max_comp.setVisibility(View.VISIBLE);
+            tvMaxComp.setText("Competidores total (entre 3 y 80): ");
         }
     }
 
-    private boolean validarGrupo() {
-        if(cantGrupos.isEmpty())
+    private boolean correctoMaxCompetidores(String tipoOrg) {
+        if(tipoOrg.contains("grupo")){
+            return inRange(3, 20, Integer.valueOf(etMaxCompetidores.getText().toString()));
+        }
+        if(tipoOrg.contains("Liga")){
+            return inRange(3, 80, Integer.valueOf(etMaxCompetidores.getText().toString()));
+        }
+        return false;
+    }
+
+    // determina si un valor se encuentra en un rango dado
+    private boolean inRange(int nroDesde, int nroHasta, int valor){
+//        Log.d("SPIN_MAX_COMP", String.valueOf(valor));
+        if(valor < nroDesde){
             return false;
+        }
+        if(valor > nroHasta){
+            return false;
+        }
         return true;
     }
 
